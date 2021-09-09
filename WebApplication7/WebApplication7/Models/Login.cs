5c70b51bc04e428d599558759e3f84782c33ce5e
huy <gymsmart121820@gmail.com>
2021-09-09 14:03:41 +0700

    p1
    hello

---
+++ WebApplication7/WebApplication7/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class Login
+    {
+        [Display(Name ="Ten dang nhap")]
+        [Required(ErrorMessage = " yeu cau ban dang nhap tai khona")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = " yeu cau ban nhap mat khau")]
+        [Display(Name = " Mat khau ")]
+        public string PassWord { get; set; }
+    }
+}
