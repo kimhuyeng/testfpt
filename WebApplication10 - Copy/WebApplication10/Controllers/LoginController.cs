5dcad930d9ea7f063d445fb3dd68eb250ebc8ad2
huy <gymsmart121820@gmail.com>
2021-09-12 20:36:44 +0700

    ver

---
+++ WebApplication10 - Copy/WebApplication10/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult announce(string thongBao)
+        {
+            ViewBag.thongBao = thongBao;
+            return View();
+        }
+        // GET: Login
+        [HttpGet]
+        public ActionResult Loggin(string password, string Username)
+        {
+            // kiểm tra tên đăng nhập và mật khẩu
+            // Username:ngokimhuy password: 123456
+            if (Username == "ngokimhuy" && password == "123456")
+            {
+                // chuyển hướng sang quanlytaikhoan
+                return Redirect("/danhMuc/QuanLyTaiKhoan");
+            }
+            return View();
+        }
+        public ActionResult Loggin(string password, string Usernam)
+        {
+            if(Username == "huymapdit" && password == 12333)
+            {
+                return Redirect("/danhMuc/QuanLyTaiKhoan")
+            }
+        }
+ }
---
+++ WebApplication10 - Copy/WebApplication10/Controllers/SeverController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication10.Controllers
+{
+    public class SeverController : Controller
+    {
+        // GET: Sever
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
