d2ed207f4bd750a8b445b160ebfc1b6a8ebe6bee
huy <gymsmart121820@gmail.com>
2021-09-12 18:07:04 +0700

    ver10

---
+++ WebApplication10/WebApplication10/Controllers/danhMucController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication10.Controllers
+{
+    public class danhMucController : Controller
+    {
+        // GET: danhMuc
+        public ActionResult QuanLyTaiKhoan(string thongBao)
+        {
+            ViewBag.thongBao = thongBao;
+            return View();
+        }
+        public ActionResult ChiTietBoCuc()
+        {
+            return View();
+        }
+        public ActionResult training()
+        {
+            return View();
+        }
+        public ActionResult trainer()
+        {
+            return View();
+        }
+    }
+}
